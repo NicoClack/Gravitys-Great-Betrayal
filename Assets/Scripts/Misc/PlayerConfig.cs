351386303ec5026ab71edb7cc94d26474df5722b
Nico Clack <nico@bn7.net>
2026-03-03 23:22:35 +0000

    Update from Newtonsoft.Json

--- Assets/Scripts/Misc/PlayerConfig.cs
+++ Assets/Scripts/Misc/PlayerConfig.cs
@@ -1,26 +1,27 @@
 using System;
 using System.IO;
 using UnityEngine;
-using Newtonsoft.Json;
 
-public class PlayerConfig {
+public class PlayerConfig
+{
 	private const string fileName = "config.json";
 
 	public int version = 1;
 	public float mouseSensitivity = 0.05f;
 
-	public static PlayerConfig Load() {
-		string configPath = Application.persistentDataPath + fileName;
-
-		if (! File.Exists(configPath)) return null;
+	public static PlayerConfig Load()
+	{
+		string configPath = Path.Combine(Application.persistentDataPath, fileName);
+		if (!File.Exists(configPath)) return null;
 
 		string stringContents = File.ReadAllText(configPath);
-		return JsonConvert.DeserializeObject<PlayerConfig>(stringContents);
+		return JsonUtility.FromJson<PlayerConfig>(stringContents);
 	}
-	public static void Save(PlayerConfig parsedContents) {
-		string configPath = Application.persistentDataPath + fileName;
 
-		string stringContents = JsonConvert.SerializeObject(parsedContents);
+	public static void Save(PlayerConfig parsedContents)
+	{
+		string configPath = Path.Combine(Application.persistentDataPath, fileName);
+		string stringContents = JsonUtility.ToJson(parsedContents, true);
 		File.WriteAllText(configPath, stringContents);
 	}
 }
