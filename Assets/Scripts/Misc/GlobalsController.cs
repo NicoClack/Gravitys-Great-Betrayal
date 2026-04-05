fb5da7c156fd6ec74b5a868fb50d38325f69e086
Nico Clack <nico@bn7.net>
2026-04-05 16:20:15 +0100

    60 FPS on Android

--- Assets/Scripts/Misc/GlobalsController.cs
+++ Assets/Scripts/Misc/GlobalsController.cs
@@ -2,22 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GlobalsController : MonoBehaviour {
+public class GlobalsController : MonoBehaviour
+{
 	[SerializeField] private ConstantData m_constants;
 
-	private void Awake() {
-		if (Globals.CurrentController != null) {
+	private void Awake()
+	{
+		if (Globals.CurrentController != null)
+		{
 			Globals.CurrentConstants = m_constants;
 
 			Destroy(gameObject);
 			return;
 		}
+#if UNITY_ANDROID
+		Application.targetFrameRate = 60;
+		QualitySettings.vSyncCount = 0;
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+#endif
 
 		Globals.CurrentController = this;
 		Globals.CurrentConstants = m_constants;
 
 		PlayerConfig config = PlayerConfig.Load();
-		if (config == null) {
+		if (config == null)
+		{
 			config = new PlayerConfig();
 			PlayerConfig.Save(config);
 		}
